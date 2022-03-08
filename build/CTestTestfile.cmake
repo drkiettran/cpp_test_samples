33a10c0ecefccd82dc9b632970ef4b451866ba74
Kiet T. Tran, Ph.d <kiet@tranmail.net>
2022-03-08 05:35:00 -0500

    initial checkin

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/cpp/test_samples
+# Build directory: /home/student/cpp/test_samples/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/student/cpp/test_samples/build/hello_test[1]_include.cmake")
+subdirs("_deps/googletest-build")
